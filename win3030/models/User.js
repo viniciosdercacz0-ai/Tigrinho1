8ccd00c27720b6a2ffa9a363b9f5d039b3d0a38e
viniciosdercacz0-ai <viniciosdercacz0@gmail.com>
2025-09-16 16:27:53 -0300

    hdxv

---
+++ win3030/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+      credits: { type: Number, default: 100 }
+      });
+
+      module.exports = mongoose.model("User", UserSchema);
+      
